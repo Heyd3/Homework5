326e8368ae7bb9b169cf3cc696a9b64b0699b59b
Evgeni <87702959+Heyd3@users.noreply.github.com>
2021-11-23 14:38:39 +0300

    Update Main.java

--- Homework/5.3/Tax/Main.java
+++ Homework/5.3/Tax/Main.java
@@ -1,28 +1,24 @@
 package Tax;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.LongAdder;
-import java.util.stream.IntStream;
-
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        ExecutorService executorService1 = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-        ExecutorService executorService2 = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-        ExecutorService executorService3 = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-        LongAdder stat = new LongAdder();
-        IntStream.range(0, 5).forEach(i -> executorService1.submit(() -> stat.add(i)));
-        IntStream.range(0, 5).forEach(i -> executorService2.submit(() -> stat.add(i)));
-        IntStream.range(0, 5).forEach(i -> executorService3.submit(() -> stat.add(i)));
 
-        executorService1.awaitTermination(3, TimeUnit.SECONDS);
-        executorService2.awaitTermination(3, TimeUnit.SECONDS);
-        executorService3.awaitTermination(3, TimeUnit.SECONDS);
-        System.out.println("\nРезультат: " + stat.sum());
-        executorService1.shutdown();
-        executorService2.shutdown();
-        executorService3.shutdown();
+        long[] diff1 = new long[]{2,3,4};
+        long[] diff2 = new long[]{20,30,40};
+        long[] diff3 = new long[]{200,300,400};
 
+        Account account = new Account();
+        Thread thread1 = new Thread(null, () -> account.transfer(diff1), "Клиент 1");
+        Thread thread2 = new Thread(null, () -> account.transfer(diff2), "Клиент 2");
+        Thread thread3 = new Thread(null, () -> account.transfer(diff3), "Клиент 3");
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        thread3.join();
+        thread2.join();
+        thread1.join();
+        System.out.println("\nБаланспослевсехпереводов: " + account.getCurrent());
     }
 }
